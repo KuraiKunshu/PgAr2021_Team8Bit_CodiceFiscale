c90eccbe0d6cb994e0798c336724d2322299dec7
Kurai Kunshu <g.sanzogni001@studenti.unibs.it>
2021-04-27 10:19:48 +0200

    creazione costruttori per ReaderXML e WriterXML

--- src/codiceFiscale/WriterXML.java
+++ src/codiceFiscale/WriterXML.java
@@ -1,4 +1,21 @@
 package codiceFiscale;
 
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.FileOutputStream;
+
 public class WriterXML {
+    XMLOutputFactory xmlof;
+    XMLStreamWriter xmlw;
+
+    public WriterXML(String filePath) {
+        try {
+            xmlof = XMLOutputFactory.newInstance();
+            xmlw = xmlof.createXMLStreamWriter(new FileOutputStream(filePath), "utf-8");
+            xmlw.writeStartDocument("utf-8", "1.0");
+        } catch (Exception e) {
+            System.out.println("Errore nell'inizializzazione del writer:");
+            System.out.println(e.getMessage());
+        }
+    }
 }
