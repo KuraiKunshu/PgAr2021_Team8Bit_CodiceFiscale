5c3d6509e46777bbad5a4add2071de370e1da795
Kurai Kunshu <g.sanzogni001@studenti.unibs.it>
2021-04-29 15:56:06 +0200

    preparazione main e aggiunta file di input xml

--- src/codiceFiscale/Main.java
+++ src/codiceFiscale/Main.java
@@ -2,6 +2,8 @@ package codiceFiscale;
 
 public class Main {
     static public void main(String[] args){
-        
+        System.out.println(System.getProperty("java.class.path")+"\\xmlFile\\comuni.xml");
+        ReaderXML lettore = new ReaderXML();
+        lettore.LeggiXMLComuni(System.getProperty("java.class.path")+"\\xmlFile\\comuni.xml");
     }
 }
