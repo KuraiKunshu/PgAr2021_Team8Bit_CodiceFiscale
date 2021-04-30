54b7e0671bceec3975f337d9826c2690b010103c
Kurai Kunshu <g.sanzogni001@studenti.unibs.it>
2021-04-30 17:06:19 +0200

    ulteriore correzione merge

--- src/codiceFiscale/Main.java
+++ src/codiceFiscale/Main.java
@@ -2,17 +2,21 @@ package codiceFiscale;
 
 public class Main {
     static public void main(String[] args){
+        //stringhe per indicere la posizione relativa dei file in input e output
         String firstPath="src/xmlFile/";
         String comuniFile="comuni.xml";
         String codiciFiscaliFile="codiciFiscali.xml";
         String inputPersoneFile="inputPersone.xml";
         String nomeOutputFile="codiciPersone.xml";
+        //input
         ReaderXML lettore = new ReaderXML();
         lettore.LeggiXMLComuni(firstPath+comuniFile);
         lettore.LeggiXMLCodiciFiscali(firstPath+codiciFiscaliFile);
         lettore.LeggiXMLInputPersone(firstPath+inputPersoneFile);
+        //output
         WriterXML scrittore = new WriterXML();
         scrittore.ScriviXML(lettore.getElenco_persone(),lettore.getElenco_codici_fiscali(),lettore.getElenco_codici_invalidi(),firstPath+nomeOutputFile);
+
         System.out.println("fine");
     }
 }
